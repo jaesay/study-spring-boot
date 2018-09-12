19e8ab85ad6fcc71941c93034b5cb2fda466df3f
jaesay <jaesay@ajou.ac.kr>
2018-09-12 22:33:13 +0900

    Test Query Methods

--- src/main/java/com/sb/persistence/BoardRepository.java
+++ src/main/java/com/sb/persistence/BoardRepository.java
@@ -1,9 +1,27 @@
 package com.sb.persistence;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import com.sb.domain.Board;
 
 public interface BoardRepository extends CrudRepository<Board, Long>{
 
+	public List<Board> findBoardByTitle(String title);
+	
+	public Collection<Board> findByWriter(String writer);
+	
+	public Collection<Board> findByWriterContaining(String writer);
+	
+	public Collection<Board> findByTitleContainingOrContentContaining(String title, String Content);
+	
+	public Collection<Board> findByTitleContainingAndBnoGreaterThan(String keyword, Long num);
+	
+	public Collection<Board> findByBnoGreaterThanOrderByBnoDesc(Long bno);
+	
+	public Page<Board> findByBnoGreaterThan(Long bno, Pageable paging);
 }
--- src/test/java/com/sb/BoardRepositoryTests.java
+++ src/test/java/com/sb/BoardRepositoryTests.java
@@ -1,11 +1,15 @@
 package com.sb;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.sb.domain.Board;
@@ -16,14 +20,53 @@ import com.sb.persistence.BoardRepository;
 public class BoardRepositoryTests {
 
 	@Autowired
-	private BoardRepository boardRepo;
+	private BoardRepository repo;
 	
 	@Test
+	public void testJPA() {
+		Pageable paging = PageRequest.of(0, 10, Sort.Direction.ASC, "bno");
+		
+		Page<Board> result = repo.findByBnoGreaterThan(0L, paging);
+		
+		System.out.println("PAGE SIZE: " + result.getSize());
+		System.out.println("TOTAL PAGES: " + result.getTotalPages());
+		System.out.println("TOTAL COUNT: " + result.getTotalElements());
+		System.out.println("NEXT: " + result.nextPageable());
+		
+		List<Board> list = result.getContent();
+		
+		list.forEach(board -> System.out.println(board));
+		
+		//Collection<Board> results = repo.findByWriterContaining("05");
+		
+		
+		/*repo.findBoardByTitle("제목200")
+		.forEach(board->System.out.println(board));*/
+		
+		//Collection<Board> results = repo.findByWriter("user00");
+		
+		//results.forEach(board->System.out.println(board));
+	}
+	
+	/*@Test
+	public void testInsert200() {
+		for(int i=1; i<=200; i++) {
+			Board board = new Board();
+			board.setTitle("제목" + i);
+			board.setContent("내용....." + i + "채우기");
+			board.setWriter("user0" + (i%10));
+			repo.save(board);
+		}
+	}*/
+	
+	
+	/*@Test
 	public void testDelete() {
 		System.out.println("DELETE Entity");
 		
 		boardRepo.deleteById(1L);
 	}
+	*/
 	
 	/*
 	@Test
